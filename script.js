4fb275353c9e91e8969158ed0330d712edf71a83
Eya Gargah <eyagargah@hotmail.com>
2022-11-05 21:18:13 +0100

    Update script.js
    -Get selected user choice in right place
    -Get house choice generated randomly

--- script.js
+++ script.js
@@ -2,6 +2,47 @@ const Close_Modal_Btn = document.querySelector('.closeBtn')
 const Rules_Modal = document.querySelector('.modal')
 const Open_Rules_Btn = document.querySelector('.openModalBtn')
 
+const userChoiceDisplay = document.querySelector('.user-pick')
+const houseChoiceDisplay = document.querySelector('.house-pick')
+const scoreDisplay = document.querySelector('.score')
+const game = document.querySelector('.game')
+const gameBtns = game.querySelectorAll('input')
+
+let userChoice
+gameBtns.forEach(gameBtn => {
+    addEventListener('click', (e)=>{
+        userChoice = e.target.id
+        userChoiceDisplay.innerHTML = userChoice
+        generateHouseChoice()
+        scoreDisplay.innerHTML = getResult()
+    })
+    
+});
+let houseChoice
+function generateHouseChoice(){
+    const randomNumber = Math.floor(Math.random() * gameBtns.length)
+    houseChoice = gameBtns[randomNumber].id
+    houseChoiceDisplay.innerHTML = houseChoice
+    
+}
+
+function getResult(){
+    let result 
+    if(userChoice=== houseChoice){
+        result ='Its a Tie'
+    }
+    if(userChoice=== houseChoice){
+        result ='Its a Tie'
+    }
+    if(userChoice=== houseChoice){
+        result ='Its a Tie'
+    }
+    return result
+}
+
+
+
+/*Modal control */
 Open_Rules_Btn.addEventListener('click',()=>{
     Rules_Modal.classList.remove('close')
     Rules_Modal.classList.add('open')
